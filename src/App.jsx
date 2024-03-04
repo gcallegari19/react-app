22c42b56f3c761e4fa2d20de33ffc70481227568
Gianni Callegari <gianninacallegari@gmail.com>
2024-03-04 16:49:42 -0300

    primer commiteo

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import MiPrimerComponente from './MiPrimerComponente'
+
+function App() {
+  
+
+  return (
+    <>
+    <h1>Hola mundo desde React</h1>
+    <p>Te queremos Vite y React</p>
+    <MiPrimerComponente></MiPrimerComponente>
+    </>
+  )
+}
+
+export default App
